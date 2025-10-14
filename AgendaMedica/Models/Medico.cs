97f80b21bf18b6c9693c76049cec8b6e48718549
eduardo45232 <eduardo.m.silva53@aluno.senai.br>
2025-10-14 12:21:03 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaMedica/Models/Medico.cs
@@ -0,0 +1,15 @@
+﻿namespace AgendaMedica.Models
+{
+    public class Medico
+    {
+        public Guid MedicoId { get; set; }
+        public string Nome { get; set; }
+        public string CRM { get; set; }
+        public string Celular { get; set; }
+
+        //chave estrangeira
+        public Guid EspecialidadeId { get; set; }//fk
+        public Especialidade Especialidade { get; set; }
+
+    }
+}
